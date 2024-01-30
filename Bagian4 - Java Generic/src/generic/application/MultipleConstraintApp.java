416abc56ca7daba9274fa1885d995c0de9055d6f
ndenawalludin <ndenawalludin101@gmail.com>
2024-01-30 22:46:30 +0700

    Bounded Type Parameter MultipleConstraint

---
+++ Bagian4 - Java Generic/src/generic/application/MultipleConstraintApp.java
@@ -0,0 +1,65 @@
+package generic.application;
+
+import javax.xml.crypto.Data;
+
+/**
+ * MultipleConstraintApp
+ */
+public class MultipleConstraintApp {
+    public static void main(String[] args) {
+        
+        // Data<Manager> managerData = new Data<>(new Manager()); ini akan error karena "Bound mismatch" yaitu class manager tidak implement CanSayHello
+
+        VicePresident president = new VicePresident();
+        president.sayHello("Nden");
+
+        Data<VicePresident> vicePresidentData = new Data<>(new VicePresident());
+    }
+
+    public static interface CanSayHello {
+        void sayHello(String name);
+    }
+
+    public static abstract class Employee {
+    
+        
+    }
+
+    public static class Manager extends Employee {
+    
+        
+    }
+
+    public static class VicePresident extends Employee implements CanSayHello{
+        @Override
+        public void sayHello(String name){
+            System.out.println("Hello " + name);
+        }
+    }
+
+    public static class Data<T extends Employee & CanSayHello> {
+        private T data;     
+        
+        
+        public Data(T data){
+            this.data = data;
+        }
+
+        /**
+         * @return T return the data
+         */
+        public T getData() {
+            return data;
+        }
+    
+        /**
+         * @param data the data to set
+         */
+        public void setData(T data) {
+            this.data = data;
+        }
+    }
+
+
+
+}
